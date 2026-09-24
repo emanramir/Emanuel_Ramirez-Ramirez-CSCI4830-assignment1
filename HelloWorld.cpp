a7cd7ab10313e28d2df43368895604b989468cce
emanramir <emanramir@gmail.com>
2026-09-23 19:37:48 -0500

    Changing the greetings

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,4 +1,4 @@
-// Simple C++ program to display "Hello World" 
+Testing git merge// Simple C++ program to display "Hello World" 
 // Header file for input output functions 
 #include<iostream>  
   
@@ -9,7 +9,7 @@ using namespace std;
 int main() 
 { 
     // prints hello world 
-    cout<<"Hello World"; 
+    cout<<"Testing git merge1!"; 
       
     return 0; 
 } 
